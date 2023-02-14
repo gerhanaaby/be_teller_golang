f3a5a6ea6f03d5a29754ed363008195cfa0148e4
syafriesya <syafriwe@gmail.com>
2023-02-14 17:27:57 +0700

    tinggal tambah header set dan repsonse api

--- controllers/skn.go
+++ controllers/skn.go
@@ -1,6 +1,11 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"teller/db"
 	"teller/models"
@@ -22,7 +27,73 @@ func PostSkn(c *gin.Context) {
 		return
 	}
 
+	PostToAPIdev(request)
+
 	c.JSON(http.StatusCreated, gin.H{
 		"message": "Yeay Berhasil!",
 	})
+
+}
+
+func PostToAPIdev(dataSKN models.Skn) map[string]string {
+
+	data := map[string]interface{}{
+		"creditAccountNo":           dataSKN.CreditAccountNo,
+		"amount":                    dataSKN.Amount,
+		"beneficiaryResidentStatus": dataSKN.BeneficiaryResidentStatus,
+		"clearingCode":              dataSKN.ClearingCode,
+		"remark":                    dataSKN.Remark,
+		"transactionDate":           dataSKN.TransactionDate,
+		"transactionTime":           dataSKN.TransactionTime,
+		"clearingTransactionCode":   dataSKN.ClearingTransactionCode,
+		"referenceId":               dataSKN.ReferenceId,
+		"paymentDetails1":           dataSKN.PaymentDetails1,
+		"senderName":                dataSKN.SenderName,
+		"paymentDetails2":           dataSKN.PaymentDetails2,
+		"paymentDetails3":           dataSKN.PaymentDetails3,
+		"debitAccountNo":            dataSKN.DebitAccountNo,
+		"beneficiaryNationStatus":   dataSKN.BeneficiaryNationStatus,
+		"beneficiaryType":           dataSKN.BeneficiaryType,
+		"beneficiaryName":           dataSKN.BeneficiaryName,
+		"chargeAmount":              dataSKN.ChargeAmount,
+		"currency":                  dataSKN.Currency,
+	}
+
+	requestJson, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &http.Client{}
+
+	request, err := http.NewRequest("POST", "https://apidev.banksinarmas.com/internal/transactions/transfer/v2.0/skn", bytes.NewBuffer(requestJson))
+	request.Header.Set("Content-type", "application/json")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dataResponse := map[string]string{}
+	err = json.Unmarshal(body, &dataResponse)
+
+	// Check your errors!
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("===============================================")
+
+	fmt.Println(string(body))
+	return dataResponse
+
 }
