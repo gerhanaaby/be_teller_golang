bdfb16cd3b961e6a1b272e3d3cc81fa5ac1578a7
fajarachmadi <fajar.d.rachmadi@gmail.com>
2023-02-16 13:35:40 +0700

    add get api list
    get api list on init

--- db/db.go
+++ db/db.go
@@ -15,13 +15,14 @@ var (
 )
 
 func ConnectDB()  error {
+	models.ApiMap = make(map[string]models.Apis)
+
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
-	os.Getenv(`PSQL_HOST`),
-	os.Getenv(`PSQL_PORT`),
+			os.Getenv(`PSQL_HOST`),
+			os.Getenv(`PSQL_PORT`),
 			os.Getenv(`PSQL_USER`),
 			os.Getenv(`PSQL_PASS`),
 			os.Getenv(`PSQL_DBNAME`))
-	fmt.Println(`PSQL---------------->`+psqlInfo)
 	db, err = gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
 	if err != nil {
 		return err
@@ -31,8 +32,14 @@ func ConnectDB()  error {
 	if err = db.Debug().Raw("SELECT * FROM apis").Scan(&models.ApiList).Error; err != nil  {
 		return err
 	}
+
+	for _, api := range models.ApiList{
+		models.ApiMap[api.Name] = api
+	}
+
 	return nil
 }
+
 func GetDB() *gorm.DB {
 	return db
 }
