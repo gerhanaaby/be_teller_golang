58ede84ef04198a96a9aef71ab668b0ea42b1b12
fajarachmadi <fajar.d.rachmadi@gmail.com>
2023-03-14 15:48:59 +0700

    Router grouping v1 & v1

--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"teller/controllers"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -17,77 +15,11 @@ func Routes() {
 		AllowHeaders: []string{"Content-Type, Authorization, access-control-allow-origin, access-control-allow-headers"},
 	}))
 
-	v1 := r.Group("v1")
+	
+	golang := r.Group("golang") 
 	{
-
-		UserRoutes := v1.Group("user") 
-		{
-			// UserRoutes.POST("/auth/login", controllers.UserLoginController)
-			UserRoutes.POST("/create", controllers.CreateUser)
-	
-			UserAuthRoutes := UserRoutes.Group("auth")
-			{
-				UserAuthRoutes.POST("/login", controllers.UserLoginController)
-			}
-	
-			UserTransRoutes := UserRoutes.Group("transac")
-			{
-				UserTransRoutes.POST("/postskn", controllers.SKN)                           ///user/transac/postskn
-				UserTransRoutes.POST("/postinquirytransfer", controllers.PostInquiryTransfer)   // inquiry transfer
-				UserTransRoutes.POST("/postinternaltransfer", controllers.PostInternalTransfer) // internal transfer
-				UserTransRoutes.POST("/postgetdetail", controllers.PostGetDetail)               // get detail
-				UserTransRoutes.POST("/postgetdetail2", controllers.PostGetDetail2)               // get detail
-				UserTransRoutes.POST("/postadvice", controllers.PostAdvice)                     // Advice
-			}
-	
-			UserNsbRoutes := UserRoutes.Group("nasabah") 
-			{
-				UserNsbRoutes.GET("/:cif/get", controllers.GetNasabahByCIF)
-			}
-	
-			UserUtilityRoutes := UserRoutes.Group("utils")
-			{
-				UserUtilityRoutes.GET("/getb64/:cif", controllers.GetBase64ByCif)
-			}
-		}
+		RunV1(golang)
+		RunV2(golang)
 	}
-
-	v2 := r.Group("v2")
-	{
-		UserRoutes := v2.Group("user")
-		{
-	
-			// UserRoutes.POST("/auth/login", controllers.UserLoginController)
-			UserRoutes.POST("/create", controllers.CreateUser)
-	
-			UserAuthRoutes := UserRoutes.Group("auth")
-			{
-				UserAuthRoutes.POST("/login", controllers.UserLoginController)
-			}
-	
-			UserTransRoutes := UserRoutes.Group("transac")
-			{
-				UserTransRoutes.POST("/postskn", controllers.SKN)                           ///user/transac/postskn
-				UserTransRoutes.POST("/postinquirytransfer", controllers.PostInquiryTransfer)   // inquiry transfer
-				UserTransRoutes.POST("/postinternaltransfer", controllers.PostInternalTransfer) // internal transfer
-				UserTransRoutes.POST("/postgetdetail", controllers.PostGetDetail)               // get detail
-				UserTransRoutes.POST("/postgetdetail2", controllers.PostGetDetail2)               // get detail
-				UserTransRoutes.POST("/postadvice", controllers.PostAdvice)                     // Advice
-			}
-	
-			UserNsbRoutes := UserRoutes.Group("nasabah") 
-			{
-				UserNsbRoutes.GET("/:cif/get", controllers.GetNasabahByCIF)
-			}
-	
-			UserUtilityRoutes := UserRoutes.Group("utils")
-			{
-				UserUtilityRoutes.GET("/getb64/:cif", controllers.GetBase64ByCif)
-			}
-		}
-	
-	}
-
-
 	r.Run(":5000")
 }
---
+++ routes/v1.go
@@ -0,0 +1,44 @@
+package routes
+
+import (
+	"teller/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RunV1(r *gin.RouterGroup) {
+	v1 := r.Group("v1")
+	{
+
+		UserRoutes := v1.Group("user")
+		{
+			// UserRoutes.POST("/auth/login", controllers.UserLoginController)
+			UserRoutes.POST("/create", controllers.CreateUser)
+
+			UserAuthRoutes := UserRoutes.Group("auth")
+			{
+				UserAuthRoutes.POST("/login", controllers.UserLoginController)
+			}
+
+			UserTransRoutes := UserRoutes.Group("transac")
+			{
+				UserTransRoutes.POST("/postskn", controllers.SKN)                               ///user/transac/postskn
+				UserTransRoutes.POST("/postinquirytransfer", controllers.PostInquiryTransfer)   // inquiry transfer
+				UserTransRoutes.POST("/postinternaltransfer", controllers.PostInternalTransfer) // internal transfer
+				UserTransRoutes.POST("/postgetdetail", controllers.PostGetDetail)               // get detail
+				UserTransRoutes.POST("/postgetdetail2", controllers.PostGetDetail2)             // get detail
+				UserTransRoutes.POST("/postadvice", controllers.PostAdvice)                     // Advice
+			}
+
+			UserNsbRoutes := UserRoutes.Group("nasabah")
+			{
+				UserNsbRoutes.GET("/:cif/get", controllers.GetNasabahByCIF)
+			}
+
+			UserUtilityRoutes := UserRoutes.Group("utils")
+			{
+				UserUtilityRoutes.GET("/getb64/:cif", controllers.GetBase64ByCif)
+			}
+		}
+	}
+}
